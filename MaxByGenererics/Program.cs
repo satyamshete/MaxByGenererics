14f0a65df0c1f6c2568e91f2bb3f6d2e11cad337
satyamshete <shetesatyam99@gmail.com>
2022-08-21 18:32:37 +0530

    [Satyam] Maximum of Three Integers Value

---
+++ MaxByGenererics/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MaxByGenererics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///UC1
+            Console.WriteLine("Find Maximum of 3 intiger numbers");
+            int input;
+            int[] intArray = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter {0} number", i + 1);
+                input = int.Parse(Console.ReadLine());
+                intArray[i] = input;
+            }
+            MaxOfThreeInt.toPrint(intArray);
+        }
+    }
+}
