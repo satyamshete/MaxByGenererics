7876ebb79fc8b72595e3ecdd2a08a0b47ea1f4a0
satyamshete <shetesatyam99@gmail.com>
2022-08-21 18:39:42 +0530

    [Satyam] Maximum of Three Float Value

---
+++ MaxByGenererics/MaxOfThreeFloat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxByGenererics
+{
+    internal class MaxOfThreeFloat
+    {
+        internal static void toPrint(float[] array)
+        {
+            float max;
+
+            max = array[0] > array[1] ? (array[0] > array[2] ? array[0] : array[2]) : (array[1] > array[2] ? array[1] : array[2]);
+
+            Console.WriteLine("Maximum of {0}, {1}, {2}  Number is: {3}", array[0], array[1], array[2], max);
+        }
+    }
+}
--- MaxByGenererics/Program.cs
+++ MaxByGenererics/Program.cs
@@ -5,16 +5,28 @@
         static void Main(string[] args)
         {
             ///UC1
-            Console.WriteLine("Find Maximum of 3 intiger numbers");
-            int input;
-            int[] intArray = new int[3];
+            //Console.WriteLine("Find Maximum of 3 intiger numbers");
+            //int input;
+            //int[] intArray = new int[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Enter {0} number", i + 1);
+            //    input = int.Parse(Console.ReadLine());
+            //    intArray[i] = input;
+            //}
+            //MaxOfThreeInt.toPrint(intArray);
+
+            ///UC2
+            Console.WriteLine("Find Maximum of 3 float numbers");
+            float input;
+            float[] floatArray = new float[3];
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine("Enter {0} number", i + 1);
-                input = int.Parse(Console.ReadLine());
-                intArray[i] = input;
+                input = float.Parse(Console.ReadLine());
+                floatArray[i] = input;
             }
-            MaxOfThreeInt.toPrint(intArray);
+            MaxOfThreeFloat.toPrint(floatArray);
         }
     }
 }
